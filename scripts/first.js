d5ef4d9959bd154b5c0da98b79a1a5ed74dcd9f0
wizard5592 <wizard5592@naver.com>
2020-09-23 10:56:04 +0900

    Add files via upload

---
+++ scripts/first.js
@@ -0,0 +1,7 @@
+/* jslint devel: true */ 
+/* eslint-disable no-console */ 
+/* eslint no-undef: "error" */ 
+/* eslint-env node */ 
+/* eslint-env browser */
+function viewCurrDate() { //    eslint-disable-line no-unused-vars        
+    document.getElementById('demo').innerHTML = Date(); }
