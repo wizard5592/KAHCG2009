c9c0f233d1d841c5f741cdf57586c9320e743ff4
wizard5592 <wizard5592@naver.com>
2020-09-28 12:28:39 +0900

    Update first.js

--- scripts/first.js
+++ scripts/first.js
@@ -5,3 +5,43 @@
 /* eslint-env browser */
 function viewCurrDate() { //    eslint-disable-line no-unused-vars        
     document.getElementById('demo').innerHTML = Date(); }
+
+function Check(){
+    var x = 10;
+    var y = 20;
+    var z = x + y;
+    var result;
+    
+    document.getElementById("test1").innerHTML = Date(z);
+    
+    x = "10";
+    y = "20";
+    z = x + y;
+    
+    document.getElementById("test2").innerHTML = Date(z);
+    
+    x = 10;
+    y = 20;
+    z = "30";
+    result = x + y + z;
+    
+    document.getElementById("test3").innerHTML = Date(result);
+    
+    x = "100";
+    y = "10";
+    z = x / y;
+    
+    document.getElementById("test4").innerHTML = Date(z);
+    
+    z = x * y;
+    
+    document.getElementById("test5").innerHTML = Date(z);
+    
+    z = x - y;
+    
+    document.getElementById("test6").innerHTML = Date(z);
+
+    z = x + y;
+    
+    document.getElementById("test7").innerHTML = Date(z);
+}
